23a45a34dee5205f70729b551c1b5fee67aa439c
aripan <aripan017@gmail.com>
2021-03-28 11:17:29 +0200

    cloned version

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJhq8yaqK10vJqybbOUZW-DBY25qo3TiQ",
+  authDomain: "burj-al-arab-91975.firebaseapp.com",
+  projectId: "burj-al-arab-91975",
+  storageBucket: "burj-al-arab-91975.appspot.com",
+  messagingSenderId: "306240846860",
+  appId: "1:306240846860:web:731b27ed7c8faa940c4229",
+};
+export default firebaseConfig;
